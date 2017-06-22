f963f1ed96035b25497707c8de05ad92d28c1240
Carrie <carrie.bilenker@gmail.com>
2017-06-21 23:22:23 -0700

    Developer boyfriend shows me text injection magic. JS prints new tasks to taskOwners

--- todo2.js
+++ todo2.js
@@ -1,11 +1,18 @@
 window.onload = function (){
   console.log("Time to get to work!");
 }
-<<<<<<<
-var tasks = []
-var yakkoTasks
 
-function createTask () {
+// create general array of tasks from form submission
+var tasks = []
+
+// create variable for workers' tasks
+var workers = {
+  "Yakko":[],
+  "Wakko":[],
+  "Dot":[]
+}
+
+function createTask() {
   event.preventDefault();
   var form = document.querySelector("form");
   var newTask = {
@@ -13,4 +20,11 @@ function createTask () {
     taskDifficulty: form.difficulty.value,
     taskInformation: form.details.value
   }
+  tasks.push(newTask);
+  console.log("New task assigned to " + newTask.taskOwner)
+  // referencing array inside of workers for selected task owner
+  // push newTask to the taskowner
+  workers[newTask.taskOwner].push(newTask);
+  $('#tasks-' + newTask.taskOwner).append(workers[newTask.taskOwner])
+  form.reset();
 }
